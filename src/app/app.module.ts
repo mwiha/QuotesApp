aa2bf81ce643c4cce04ee923f84fe44919b1b034
Alice Mwihaki <alicemwihaki99@gmail.com>
2019-11-29 17:35:53 +0300

    generated component goal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent
   ],
   imports: [
     BrowserModule,
